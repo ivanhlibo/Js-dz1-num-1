605deb7117621f9d467d81ed3b0cdaded1286a70
Ivan2007 <ivanhlibov2007@gmail.com>
2022-01-15 09:37:06 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+let frag = true;
+let txt = "Привет";
+let num = 123;
+let string = "true";
+
+
+console.log("Bolean is", typeof frag)
+console.log("Txt is ", typeof txt  )
+console.log("Num is ", typeof num )
+console.log("String is ", typeof string)
+
+
+let name ="Генератор защитного поля";
+let price = 1000;
+console.log('Обрано" ' + name + '", цена за штуку' + price + '   кредит '  );
+
+
+
+
+
+
